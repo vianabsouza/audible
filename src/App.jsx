e0c06e2ec6790686406633aa3eb9296f7116653a
Barbara Viana <vianabsouza@gmail.com>
2024-06-20 19:32:35 -0300

    create book cover component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css'
+import BookCover from './BookCover';
+import brasCubasBookCover from './assets/bras-cubas-book-cover.jpeg'
 
 function App() {
+  const bookInfo = {
+    name: 'Memórias Póstumas de Brás Cubas',
+    author: "Machado de Assis",
+    totalChapters: 2,
+    bookCover: brasCubasBookCover,
+    alternativeText: 'Capa do livro Memórias Póstumas de Brás Cubas'
+  }
+
   return (
     <>
+      <BookCover bookCover={bookInfo.bookCover} alternativeText={bookInfo.alternativeText}/>
     </>
   )
 }
